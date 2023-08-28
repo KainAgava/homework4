3746318e56e3f255a05704b065bf3b7b37e2d977
KainAgava <143312166+KainAgava@users.noreply.github.com>
2023-08-28 14:19:44 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name = "Петров";
+            string surname = "Пётр";
+           
+           string correctName = surname;
+           string correctSurname = name;
+
+             name = correctName;
+             surname = correctSurname;
+
+                            Console.WriteLine($"Имя: " + name + " Фамилия:" + surname + ""); 
+               
+           
+
+        }
+
+    }
+}
